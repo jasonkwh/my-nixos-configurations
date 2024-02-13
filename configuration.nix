79f232359ea3d012bc7a48aeb8ca85d5ca556629
Jason Huang <jasonkwh@gmail.com>
2024-02-13 18:36:22 +1100

    trying to use separate file for kernel selection

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,9 @@
 
 { config, pkgs, lib, ... }:
 
+let
+  linuxKernel = import ./kernel.nix { inherit pkgs; };
+in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -12,21 +15,7 @@
 
   # Bootloader.
   boot = {
-    # compile linux kernel myself,
-    # because the binary from nix repository is a bit old...
-    kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_latest.override {
-      argsOverride = rec {
-        src = pkgs.fetchurl {
-              url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-              sha256 = "f68d9f5ffc0a24f850699b86c8aea8b8687de7384158d5ed3bede37de098d60c";
-        };
-        version = "6.7.4";
-        modDirVersion = "6.7.4";
-        };
-    });
-
-    # using NixOS prebuilt kernel
-    # kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = linuxKernel;
 
     loader = {
       systemd-boot = {
