36c4b41b205a74f4c6f844c66aaa33f7508332d9
Jason Huang <jasonkwh@gmail.com>
2024-02-18 00:55:57 +1100

    update the kernel

--- kernel.nix
+++ kernel.nix
@@ -1,17 +1,19 @@
 { pkgs }: 
 
 let
+  # set the configurations here
   compileKernel = true;
+  kernelVersion = "6.7.5";
   
   kernelPackages = if compileKernel then
     pkgs.linuxPackagesFor (pkgs.linux_latest.override {
       argsOverride = rec {
         src = pkgs.fetchurl {
           url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-          sha256 = "f68d9f5ffc0a24f850699b86c8aea8b8687de7384158d5ed3bede37de098d60c";
+          sha256 = "29f6464061b8179cbb77fc5591e06a2199324e018c9ed730ca3e6dfb145539ff";
         };
-        version = "6.7.4";
-        modDirVersion = "6.7.4";
+        version = kernelVersion;
+        modDirVersion = kernel;
       };
     })
   else
