8cf09ae83c7fe204c4dc741208550bdf3b3b9e8c
Jason Huang <jasonkwh@gmail.com>
2024-02-26 14:05:05 +1100

    update to kernel 6.7.6

--- kernel.nix
+++ kernel.nix
@@ -3,14 +3,14 @@
 let
   # set the configurations here
   compileKernel = true;
-  kernelVersion = "6.7.5";
+  kernelVersion = "6.7.6";
   
   kernelPackages = if compileKernel then
     pkgs.linuxPackagesFor (pkgs.linux_latest.override {
       argsOverride = rec {
         src = pkgs.fetchurl {
           url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-          sha256 = "29f6464061b8179cbb77fc5591e06a2199324e018c9ed730ca3e6dfb145539ff";
+          sha256 = "e489ec0e1370d089b446d565aded7a698093d2b7c4122a18f21edb6ef93d37d3";
         };
         version = kernelVersion;
         modDirVersion = kernelVersion;
