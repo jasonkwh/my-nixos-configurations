9288b4527254f0e5ff145a3dd9e4017a0a32d22e
Jason Huang <jasonkwh@users.noreply.github.com>
2024-03-12 11:56:57 +1100

    update to kernel 6.8

--- kernel.nix
+++ kernel.nix
@@ -3,14 +3,14 @@
 let
   # set the configurations here
   compileKernel = true;
-  kernelVersion = "6.7.8";
+  kernelVersion = "6.8";
   
   kernelPackages = if compileKernel then
     pkgs.linuxPackagesFor (pkgs.linux_latest.override {
       argsOverride = rec {
         src = pkgs.fetchurl {
           url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-          sha256 = "469ff46b98685df13b56c98417c64ba7a30f8a45baf34aa99f07935e1bf65c18";
+          sha256 = "c969dea4e8bb6be991bbf7c010ba0e0a5643a3a8d8fb0a2aaa053406f1e965f3";
         };
         version = kernelVersion;
         modDirVersion = kernelVersion;
