e3de6aff0162d6fb3bd1389dffca2f3af5c67e27
Jason Huang <jasonkwh@gmail.com>
2024-03-16 23:17:05 +1100

    update to kernel 6.8.1

--- kernel.nix
+++ kernel.nix
@@ -2,15 +2,15 @@
 
 let
   # set the configurations here
-  compileKernel = false;
-  kernelVersion = "6.8";
+  compileKernel = true;
+  kernelVersion = "6.8.1";
   
   kernelPackages = if compileKernel then
     pkgs.linuxPackagesFor (pkgs.linux_latest.override {
       argsOverride = rec {
         src = pkgs.fetchurl {
           url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-          sha256 = "c969dea4e8bb6be991bbf7c010ba0e0a5643a3a8d8fb0a2aaa053406f1e965f3";
+          sha256 = "8d0c8936e3140a0fbdf511ad7a9f21121598f3656743898f47bb9052d37cff68";
         };
         version = kernelVersion;
         modDirVersion = kernelVersion;
