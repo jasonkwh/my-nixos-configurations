d0031e0306377d7a30527ce96ac1238f814d6397
Jason Huang <jasonkwh@users.noreply.github.com>
2024-05-28 18:23:07 +1000

    update kernel to 6.9.2

--- kernel.nix
+++ kernel.nix
@@ -3,14 +3,14 @@
 let
   # set the configurations here
   compileKernel = false;
-  kernelVersion = "6.8.1";
+  kernelVersion = "6.9.2";
   
   kernelPackages = if compileKernel then
     pkgs.linuxPackagesFor (pkgs.linux_latest.override {
       argsOverride = rec {
         src = pkgs.fetchurl {
           url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-          sha256 = "8d0c8936e3140a0fbdf511ad7a9f21121598f3656743898f47bb9052d37cff68";
+          sha256 = "d46c5bdf2c5961cc2a4dedefe0434d456865e95e4a7cd9f93fff054f9090e5f9";
         };
         version = kernelVersion;
         modDirVersion = kernelVersion;
