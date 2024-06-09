b6bb8d40e9cdb86ec5ef9dff197c2f7dd60fd705
Jason Huang <jasonkwh@gmail.com>
2024-06-09 16:13:47 +1000

    enable k3s master node, and disable swap for k3s

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -25,10 +25,10 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [{
-    device = "/swapfile";
-    size = 8 * 1024; # 8GB
-  }];
+  # swapDevices = [{
+  #   device = "/swapfile";
+  #   size = 8 * 1024; # 8GB
+  # }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
