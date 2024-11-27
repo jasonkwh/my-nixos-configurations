90935516db07cb4f37b5bf870fe47bc4999591ba
Jason Huang <jason.huang@betmakers.com>
2024-11-27 21:29:35 +0100

    update syntax, use go 1.23

--- home.nix
+++ home.nix
@@ -4,43 +4,45 @@
   home.username = "jasonkwh";
   home.homeDirectory = "/home/jasonkwh";
 
-  # basic configuration of git, please change to your own
-  programs.git = {
-    enable = true;
-    userName = "Jason Huang";
-    userEmail = "jasonkwh@gmail.com";
-  };
-
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      ll = "ls -l --color=auto";
-      kc = "kubectl";
-      python = "python3";
-      vi = "nvim";
-    };
-    history = {
-      size = 10000;
-      path = "${config.xdg.dataHome}/zsh/history";
-    };
-    initExtra = "
-      export KUBECONFIG=~/.kube/config
-      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-      eval $(thefuck --alias)
-    ";
-    zplug = {
+  programs = {
+    # basic configuration of git, please change to your own
+    git = {
       enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; }
-        { name = "zsh-users/zsh-syntax-highlighting"; }
-        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-      ];
+      userName = "Jason Huang";
+      userEmail = "jasonkwh@gmail.com";
     };
-  };
 
-  programs.bash = {
-    enable = true;
-    initExtra = "exec zsh";
+    zsh = {
+      enable = true;
+      shellAliases = {
+        ll = "ls -l --color=auto";
+        kc = "kubectl";
+        python = "python3";
+        vi = "nvim";
+      };
+      history = {
+        size = 10000;
+        path = "${config.xdg.dataHome}/zsh/history";
+      };
+      initExtra = "
+        export KUBECONFIG=~/.kube/config
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+        eval $(thefuck --alias)
+      ";
+      zplug = {
+        enable = true;
+        plugins = [
+          { name = "zsh-users/zsh-autosuggestions"; }
+          { name = "zsh-users/zsh-syntax-highlighting"; }
+          { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+        ];
+      };
+    };
+
+    bash = {
+      enable = true;
+      initExtra = "exec zsh";
+    };
   };
 
   # Packages that should be installed to the user profile.
@@ -69,7 +71,7 @@
     # programming
     neovim
     gh
-    go_1_22
+    go_1_23
     cargo
     rustc
     python3
