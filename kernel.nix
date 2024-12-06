951d90f4ea1794a252207e8de6d82fca181ef3b5
Jason Huang <jasonkwh@users.noreply.github.com>
2024-12-06 23:30:04 +0100

    update to Linux kernel 6.12.3

--- kernel.nix
+++ kernel.nix
@@ -3,14 +3,14 @@
 let
   # set the configurations here
   compileKernel = false;
-  kernelVersion = "6.12.1";
+  kernelVersion = "6.12.3";
   
   kernelPackages = if compileKernel then
     pkgs.linuxPackagesFor (pkgs.linux_latest.override {
       argsOverride = rec {
         src = pkgs.fetchurl {
           url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-          sha256 = "0193b1d86dd372ec891bae799f6da20deef16fc199f30080a4ea9de8cef0c619";
+          sha256 = "c89809cc777d50f1ea484a118630281a26383707a0e752c96fd834f6e765deae";
         };
         version = kernelVersion;
         modDirVersion = kernelVersion;
