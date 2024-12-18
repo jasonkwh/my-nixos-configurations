8337e3fc4ca6c2de3ce9443cc7d7106e389d24fa
Jason Huang <jasonkwh@users.noreply.github.com>
2024-12-18 10:14:59 +0100

    stick with kernel 6.6.63

--- kernel.nix
+++ kernel.nix
@@ -2,21 +2,21 @@
 
 let
   # set the configurations here
-  compileKernel = false;
-  kernelVersion = "6.12.3";
+  compileKernel = true;
+  kernelVersion = "6.6.63";
   
   kernelPackages = if compileKernel then
     pkgs.linuxPackagesFor (pkgs.linux_latest.override {
       argsOverride = rec {
         src = pkgs.fetchurl {
           url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-          sha256 = "c89809cc777d50f1ea484a118630281a26383707a0e752c96fd834f6e765deae";
+          sha256 = "d1054ab4803413efe2850f50f1a84349c091631ec50a1cf9e891d1b1f9061835";
         };
         version = kernelVersion;
         modDirVersion = kernelVersion;
       };
     })
   else
-    pkgs.linuxPackages_6_6;
+    pkgs.linuxPackages_latest;
 in
   kernelPackages
