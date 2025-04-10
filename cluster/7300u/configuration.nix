20c2dca4a5bf8e715df2ed0c986c75be369e75b2
Jason Huang <jasonkwh@gmail.com>
2025-04-10 21:47:02 +0200

    try flannel iface

--- cluster/7300u/configuration.nix
+++ cluster/7300u/configuration.nix
@@ -239,9 +239,9 @@ in
       role = "agent"; # or agent
       serverAddr = "https://10.214.121.80:6443";
       token = "K107554bc617e907cf70466a0af218deb9c9ae15f18a29b3033da9583b51be61f6e::server:f9a22d3080e522af42b6e380c413b17d";
-      extraFlags = toString [
-        # "--kubelet-arg=v=4" # Optionally add additional args to k3s
-      ];
+        extraFlags = [
+          "--flannel-iface=wlp58s0"
+        ];
     };
 
     logind = {
