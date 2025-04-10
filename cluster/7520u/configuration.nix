5b65835ae68d1f37237675c6edcddf3691668a21
Jason Huang <jasonkwh@users.noreply.github.com>
2025-04-10 22:07:03 +0200

    update 7520u cluster settings

--- cluster/7520u/configuration.nix
+++ cluster/7520u/configuration.nix
@@ -49,13 +49,11 @@ in
 
     firewall = {
       allowedTCPPorts = [
-        6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
-        # 10250 # kubelet metrics
-        # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-        # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+        6443   # Kubernetes API server (required)
+        10250  # Kubelet API (optional but recommended for metrics/logs/debugging)
       ];
       allowedUDPPorts = [
-        # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+        8472   # Flannel VXLAN (required for inter-node pod networking)
       ];
     };
   };
