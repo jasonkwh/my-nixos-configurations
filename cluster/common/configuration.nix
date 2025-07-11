2b09f4957eab61a4a72856bac8c23fcb92bcb42f
Jason Huang <jasonkwh@gmail.com>
2025-07-11 11:06:20 +0200

    do some refactor

---
+++ cluster/common/configuration.nix
@@ -0,0 +1,266 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  # Bootloader.
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 10; # don't keep too much generations
+      };
+
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  nix = {
+    settings = {
+      sandbox = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+      system-features = [ "kvm" ];
+    };
+  };
+
+  networking = {
+    networkmanager = {
+      enable = true; # Enable networking
+
+      wifi = {
+        powersave = false;
+      };
+    };
+
+    firewall = {
+      allowedTCPPorts = [
+        6443   # Kubernetes API server (required)
+        10250  # Kubelet API (optional but recommended for metrics/logs/debugging)
+      ];
+      allowedUDPPorts = [
+        8472   # Flannel VXLAN (required for inter-node pod networking)
+      ];
+    };
+  };
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_AU.UTF-8";
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_AU.UTF-8";
+      LC_IDENTIFICATION = "en_AU.UTF-8";
+      LC_MEASUREMENT = "en_AU.UTF-8";
+      LC_MONETARY = "en_AU.UTF-8";
+      LC_NAME = "en_AU.UTF-8";
+      LC_NUMERIC = "en_AU.UTF-8";
+      LC_PAPER = "en_AU.UTF-8";
+      LC_TELEPHONE = "en_AU.UTF-8";
+      LC_TIME = "en_AU.UTF-8";
+    };
+  };
+
+  # Use this for the kssshaskpass
+  # programs.ssh.askPassword = lib.mkForce "${pkgs.plasma5Packages.ksshaskpass}/bin/ksshaskpass";
+  # or this for seahorse
+  # programs.ssh.askPassword = lib.mkForce "${pkgs.gnome.seahorse}/libexec/seahorse/ssh-askpass";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users = {
+    jasonkwh = {
+      isNormalUser = true;
+      description = "Jason Huang";
+      extraGroups = [ "networkmanager" "wheel" "podman" ];
+    };
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  nixpkgs.config = {
+  # Allow unfree packages
+    allowUnfree = true;
+
+    permittedInsecurePackages = [
+      "electron-33.4.11"
+    ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    curl
+    xwayland
+    gparted
+    coreutils
+    gcc
+    gdb
+    gnumake
+    binutils
+    bc
+    file
+    nixVersions.latest
+    kdePackages.yakuake
+    kdePackages.konqueror
+    kdePackages.ktorrent
+    wineWowPackages.full
+    winetricks
+  ];
+
+  environment.variables.EDITOR = "vim";
+
+  virtualisation = {
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+    # virtualbox = {
+    #   host = {
+    #     enable = true;
+    #     enableExtensionPack = true;
+    #   };
+    #   guest = {
+    #     enable = true;
+    #   };
+    # };
+  };
+
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.noto
+      nerd-fonts.jetbrains-mono
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      source-code-pro
+      source-han-mono
+      source-han-sans
+      source-han-serif
+      wqy_zenhei
+    ];
+
+    fontDir.enable = true;
+
+    fontconfig = {
+      enable = true;
+
+      # Fixes pixelation
+      antialias = true;
+
+      # Fixes antialiasing blur
+      hinting = {
+        enable = true;
+      };
+    };
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+
+  programs.dconf = {
+    enable = true;
+  };
+
+  # List services that you want to enable:
+
+  services = {
+    displayManager = {
+      sddm = {
+        enable = true;
+      };
+    };
+      
+    desktopManager = {
+      plasma6 = {
+        enable = true;
+        enableQt5Integration = true;
+      };
+    };
+
+    xserver = {
+      # Enable the X11 windowing system.
+      enable = true;
+
+      # Configure keymap in X11
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+
+    printing = {
+      enable = true; # Enable CUPS to print documents.
+      openFirewall = true;
+    };
+
+    openssh = {
+      enable = true; # Enable the OpenSSH daemon.
+      settings = {
+        X11Forwarding = true;
+        PermitRootLogin = "yes";
+        PasswordAuthentication = true;
+      };
+      openFirewall = true;
+    };
+
+    logind = {
+      lidSwitch = "ignore";
+      lidSwitchDocked = "ignore";
+      lidSwitchExternalPower = "ignore";
+    };
+  };
+
+  hardware = {    
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    steam-hardware = {
+      enable = true;
+    };
+
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
