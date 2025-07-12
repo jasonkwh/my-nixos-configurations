85566507192c9cac74852bd910cc5ac09ced741f
Jason Huang <jasonkwh@gmail.com>
2025-07-12 12:34:22 +0200

    Fix DNS not resolve issue while using Surfshark VPN

--- cluster/common/configuration.nix
+++ cluster/common/configuration.nix
@@ -29,8 +29,10 @@
   };
 
   networking = {
+    nameservers = [ "1.1.1.1" "1.0.0.1" ];
     networkmanager = {
       enable = true; # Enable networking
+      dns = "none";
 
       wifi = {
         powersave = false;
