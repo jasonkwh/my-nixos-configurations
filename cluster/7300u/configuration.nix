6953d05e193b0e41c00ec468fdcb444500f68302
Jason Huang <jasonkwh@gmail.com>
2025-07-29 23:07:47 +0200

    Enable avahi for mDNS

--- cluster/7300u/configuration.nix
+++ cluster/7300u/configuration.nix
@@ -25,11 +25,16 @@
     k3s = {
       enable = true;
       role = "agent"; # or agent
-      serverAddr = "https://10.63.198.80:6443";
+      serverAddr = "https://jasonkwh-7520u.local:6443";
       token = "K107554bc617e907cf70466a0af218deb9c9ae15f18a29b3033da9583b51be61f6e::server:f9a22d3080e522af42b6e380c413b17d";
         extraFlags = [
           "--flannel-iface=wlp58s0"
         ];
+    };
+
+    avahi = {
+      enable = true;
+      nssmdns4 = true;  # Enable mDNS name resolution in the NSS layer
     };
   };
 }
