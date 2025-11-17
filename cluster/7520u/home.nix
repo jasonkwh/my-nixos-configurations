1fe1f9e1196a5a8b413d20a118540935918ba827
Jason Huang <jasonkwh@users.noreply.github.com>
2025-11-18 00:28:25 +0100

    Add googleearth-pro and cursor packages; update home.nix and configuration.nix

--- cluster/7520u/home.nix
+++ cluster/7520u/home.nix
@@ -1,5 +1,18 @@
 { config, pkgs, ... }:
 
+let
+  cursor = pkgs.appimageTools.wrapType2 {
+    pname = "cursor";
+    version = "1.7.36";
+
+    src = pkgs.fetchurl {
+      url = "https://downloads.cursor.com/production/493c403e4a45c5f971d1c76cc74febd0968d57d8/linux/x64/Cursor-1.7.36-x86_64.AppImage";
+      sha256 = "sha256-zY9kM9td0yKAMxVmad7saN4c6z2p5OFEa7ScCA3Qo3I=";
+    };
+
+    extraPkgs = pkgs: with pkgs; [ ];
+  };
+in
 {
   home.packages = with pkgs; [
     mysql-workbench
@@ -14,7 +27,11 @@
     discord
     slack
     warp-terminal
-    code-cursor
+    cursor
+    googleearth-pro
+    ffmpeg
+    shntool
+    flac
 
     # games
     steam
