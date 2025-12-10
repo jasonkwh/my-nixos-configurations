4007c41db552a94a631ddcad34a0608e9fc4e875
Jason Huang <jasonkwh@users.noreply.github.com>
2025-12-10 16:44:03 +0100

    Add subUidRanges and subGidRanges for user configuration in configuration.nix

--- cluster/common/configuration.nix
+++ cluster/common/configuration.nix
@@ -92,6 +92,8 @@
       description = "Jason Huang";
       extraGroups = [ "networkmanager" "wheel" "podman" ];
       shell = pkgs.zsh;
+      subUidRanges = [{ startUid = 100000; count = 65536; }];
+      subGidRanges = [{ startGid = 100000; count = 65536; }];
     };
   };
 
