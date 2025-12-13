d664916af39a5595267857a4f0dcd8b2352f181f
Jason Huang <jasonkwh@gmail.com>
2025-12-13 12:23:54 +0100

    Remove Avahi configuration from 7300u and 7520u settings, while adding it to the common configuration for improved consistency across clusters.

--- cluster/7520u/configuration.nix
+++ cluster/7520u/configuration.nix
@@ -56,16 +56,6 @@
       ];
     };
 
-    avahi = {
-      enable = true;
-      nssmdns4 = true;  # Enable mDNS name resolution in the NSS layer
-      publish = {
-        enable = true;
-        addresses = true;  # Publish the host's IP addresses
-        workstation = true;  # Publish the workstation service
-      };
-    };
-
     # use evtest to find out the device id & key num
     udev.extraHwdb = ''
       evdev:atkbd:*
