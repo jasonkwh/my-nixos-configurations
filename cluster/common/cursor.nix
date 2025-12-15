ca318942a2d025960335203d1df0b4554c564db5
Jason Huang <jasonkwh@gmail.com>
2025-12-15 11:48:35 +0100

    Refactor cursor.nix to improve packaging structure by introducing a let binding for appimageContents and wrappedCursor, enhancing installation process and dependency management for the Cursor code editor.

--- cluster/common/cursor.nix
+++ cluster/common/cursor.nix
@@ -1,6 +1,6 @@
 { pkgs }:
 
-pkgs.appimageTools.wrapType2 {
+let
   pname = "cursor";
   version = "2.2.20";
 
@@ -10,54 +10,99 @@ pkgs.appimageTools.wrapType2 {
     sha256 = "sha256-dY42LaaP7CRbqY2tuulJOENa+QUGSL09m07PvxsZCr0=";
   };
 
-  extraPkgs = pkgs: with pkgs; [
-    # Core libraries for Electron/native modules
-    libxkbcommon
-    xorg.libX11
-    xorg.libXrandr
-    xorg.libXcursor
-    xorg.libXi
-    xorg.libXdamage
-    xorg.libXcomposite
-    xorg.libXfixes
-    xorg.libXtst
-    xorg.libxcb
-    xorg.libxshmfence
+  appimageContents = pkgs.appimageTools.extractType2 { inherit pname version src; };
 
-    # Audio/video
-    alsa-lib
-    pipewire
-    pulseaudio
-    libpulseaudio
+  wrappedCursor = pkgs.appimageTools.wrapType2 {
+    inherit pname version src;
 
-    # Graphics
-    mesa
-    libGL
-    libdrm
-    vulkan-loader
+    extraPkgs = pkgs: with pkgs; [
+      # Core libraries for Electron/native modules
+      libxkbcommon
+      xorg.libX11
+      xorg.libXrandr
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXdamage
+      xorg.libXcomposite
+      xorg.libXfixes
+      xorg.libXtst
+      xorg.libxcb
+      xorg.libxshmfence
 
-    # GTK/desktop integration
-    gtk3
-    glib
-    pango
-    cairo
-    gdk-pixbuf
-    atk
-    at-spi2-atk
-    at-spi2-core
-    dbus
+      # Audio/video
+      alsa-lib
+      pipewire
+      pulseaudio
+      libpulseaudio
 
-    # Other dependencies
-    nss
-    nspr
-    cups
-    expat
-    libxkbfile
-    systemd
-    libsecret
-    gnome-keyring
+      # Graphics
+      mesa
+      libGL
+      libdrm
+      vulkan-loader
 
-    # For native modules
-    stdenv.cc.cc.lib
-  ];
+      # GTK/desktop integration
+      gtk3
+      glib
+      pango
+      cairo
+      gdk-pixbuf
+      atk
+      at-spi2-atk
+      at-spi2-core
+      dbus
+
+      # Other dependencies
+      nss
+      nspr
+      cups
+      expat
+      libxkbfile
+      systemd
+      libsecret
+      gnome-keyring
+
+      # For native modules
+      stdenv.cc.cc.lib
+    ];
+  };
+in
+pkgs.stdenv.mkDerivation {
+  inherit pname version;
+
+  src = wrappedCursor;
+
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    mkdir -p $out/share/applications
+    mkdir -p $out/share/icons/hicolor/512x512/apps
+
+    # Create wrapper with flags to suppress VSync errors
+    makeWrapper ${wrappedCursor}/bin/cursor $out/bin/cursor \
+      --add-flags "--disable-gpu-vsync" \
+      --add-flags "--enable-features=UseOzonePlatform" \
+      --add-flags "--ozone-platform-hint=auto"
+
+    # Copy desktop file and icon
+    cp ${appimageContents}/cursor.desktop $out/share/applications/cursor.desktop
+    cp ${appimageContents}/cursor.png $out/share/icons/hicolor/512x512/apps/cursor.png
+
+    # Fix desktop file paths
+    substituteInPlace $out/share/applications/cursor.desktop \
+      --replace-fail "Exec=cursor" "Exec=$out/bin/cursor" \
+      --replace-fail "Icon=cursor" "Icon=$out/share/icons/hicolor/512x512/apps/cursor.png"
+
+    runHook postInstall
+  '';
+
+  meta = with pkgs.lib; {
+    description = "Cursor - The AI-first Code Editor";
+    homepage = "https://cursor.com";
+    platforms = [ "x86_64-linux" ];
+    mainProgram = "cursor";
+  };
 }
