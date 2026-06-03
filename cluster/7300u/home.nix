87bdf529db3175db49118b5799c37ceb467d2779
Jason Huang <jasonkwh@gmail.com>
2026-06-03 13:48:44 +0200

    Add power management configuration for laptops in home.nix files; remove cursor package from common home-gui.nix and replace with code-cursor for improved functionality.

--- cluster/7300u/home.nix
+++ cluster/7300u/home.nix
@@ -1,10 +1,21 @@
 { config, pkgs, ... }:
 
 {
-  # Add user-specific packages here.
+  home.file.".config/powermanagementprofilesrc" = {
+    text = ''
+      [AC]
+      lidAction=0
+
+      [Battery]
+      lidAction=0
+
+      [LowBattery]
+      lidAction=0
+    '';
+    force = true;
+  };
+
   home.packages = with pkgs; [
     thunderbird
   ];
-
-  # Add machine-specific home-manager settings here.
 }
--- cluster/7520u/home.nix
+++ cluster/7520u/home.nix
@@ -1,6 +1,20 @@
 { config, pkgs, ... }:
 
 {
+  home.file.".config/powermanagementprofilesrc" = {
+    text = ''
+      [AC]
+      lidAction=0
+
+      [Battery]
+      lidAction=0
+
+      [LowBattery]
+      lidAction=0
+    '';
+    force = true;
+  };
+
   home.packages = with pkgs; [
     discord
     slack
