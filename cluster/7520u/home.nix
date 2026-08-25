750b41b5925b004b0e450610650d9ce5cacca996
Jason Huang <jasonkwh@users.noreply.github.com>
2026-08-25 19:42:31 +1000

    refactor: drop redundant laptop comments from per-host home.nix

--- cluster/7520u/home.nix
+++ cluster/7520u/home.nix
@@ -1,9 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Laptop-shared home configuration (power management profiles, etc.) lives
-  # in ../common/home-laptop.nix.
-
   home.packages = with pkgs; [
     discord
     slack
