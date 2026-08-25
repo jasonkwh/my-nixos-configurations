65bdab1ef608a8233e36c8ebfb51f96adf8b5584
jasonkwh <noreply@github.com>
2026-08-26 01:15:00 +1000

    common/home: export HERMES_MACHINE_IDENTITY from hostname

--- cluster/common/home.nix
+++ cluster/common/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, username, fullName, email, homeDirectory, isLaptop ? false, ... }:
+{ config, pkgs, lib, username, fullName, email, homeDirectory, isLaptop ? false, osConfig ? {}, ... }:
 
 {
   imports = lib.optionals isLaptop [ ./home-laptop.nix ];
@@ -15,6 +15,7 @@
     sessionVariables = {
       KUBECONFIG = "${config.home.homeDirectory}/.kube/config";
       EDITOR = "vim";
+      HERMES_MACHINE_IDENTITY = "xiaoshengsheng @ ${osConfig.networking.hostName or "unknown"}";
       # ELECTRON_OZONE_PLATFORM_HINT (Wayland for Electron apps) is
       # laptop-only: see home-laptop.nix.
     };
